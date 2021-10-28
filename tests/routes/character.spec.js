dac49ffdba12ba68203c520fe4406af0ae616279
sebastian <eduardociare871@gmail.com>
2021-10-28 00:02:50 -0300

    three commit

--- tests/routes/character.spec.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const request = require('supertest');
-const app = require('../../src/app.js');
-
-// describe('/characters', ()=> {
-//     it('')
-// })
